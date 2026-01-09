7c6beef0d55bd0214a49043621b085dff53ee511
Nursultan <Nnnursultan9506@gmail.com>
2026-01-09 08:20:44 +0500

    Refactor ContactsMain component styles to simplify layout and enhance readability. Update CSS to remove unnecessary elements, adjust margins, and improve responsiveness across screen sizes. Replace Row component with a more semantic heading for better accessibility.

--- src/components/ContactsMain/ContactsMain.jsx
+++ src/components/ContactsMain/ContactsMain.jsx
@@ -1,17 +1,13 @@
-import { Row } from "antd"
 import { useTranslation } from 'react-i18next';
-import Contact from '../../assets/images/Contact.svg'
 import './ContactsMain.css'
 
 const Main = () => {
   const { t } = useTranslation();
   return (
-    <div className="additional-contacts-container animate-fade-soft">
-     <Row className="about-content">
-          <div className="about-content-text">
-               {t('contactTitle')}
-          </div>
-      </Row>  
+    <div className="additional-contacts-container">
+      <h1 className="contact-content-text">
+        {t('contactTitle')}
+      </h1>
     </div>
   )
 }
